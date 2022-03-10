e98b78e7d4315d930a69cd37ba5e112ea3862d20
Andris Volkovs <anvolkovs@gmail.com>
2022-03-10 14:23:03 +0000

    Quick update to test basic functinality.

--- SearchServer/src/main.cpp
+++ SearchServer/src/main.cpp
@@ -14,30 +14,23 @@
 using namespace std::string_literals;
 using namespace read_input;
 
+void BasicFunctionality(){
+    // Creating server with "minus words"
+    SearchServer search_server("and in on"s);
+
+    // Adding documents
+    AddDocument(search_server, 1, "furry cat furry tail"s, DocumentStatus::kActual, {7, 2, 7});
+    AddDocument(search_server, 2, "furry dog and cool belt"s, DocumentStatus::kActual, {1, 2});
+    AddDocument(search_server, 3, "big dog bird evgeny"s, DocumentStatus::kActual, {1, 3, 2});
+    AddDocument(search_server, 4, "big dog bird dog is not real"s, DocumentStatus::kActual, {1, 1, 1});
+    AddDocument(search_server, 5, "cartoon dogs wasn't there"s, DocumentStatus::kActual, {1, 2});
+
+    // Looking for documents that contain requested words
+    FindTopDocuments(search_server, "furry dog"s);
+}
+
 int main() {
-    SearchServer search_server("and with"s);
-
-    search_server.AddDocument(1, "funny pet and nasty rat"s, DocumentStatus::kActual, {7, 2, 7});
-
-    search_server.AddDocument(2, "funny pet with curly hair"s, DocumentStatus::kActual, {1, 2});
-
-    search_server.AddDocument(3, "funny pet with curly hair"s, DocumentStatus::kActual, {1, 2});
-
-    search_server.AddDocument(4, "funny pet and curly hair"s, DocumentStatus::kActual, {1, 2});
-
-    search_server.AddDocument(5, "funny funny pet and nasty nasty rat"s, DocumentStatus::kActual, {1, 2});
-
-    search_server.AddDocument(6, "funny pet and not very nasty rat"s, DocumentStatus::kActual, {1, 2});
-
-    search_server.AddDocument(7, "very nasty rat and not very funny pet"s, DocumentStatus::kActual, {1, 2});
-
-    search_server.AddDocument(8, "pet with rat and rat and rat"s, DocumentStatus::kActual, {1, 2});
-
-    search_server.AddDocument(9, "nasty rat with curly hair"s, DocumentStatus::kActual, {1, 2});
-
-    std::cout << "Before duplicates removed: "s << search_server.GetDocumentCount() << std::endl;
-    RemoveDuplicates(search_server);
-    std::cout << "After duplicates removed: "s << search_server.GetDocumentCount() << std::endl;
+    BasicFunctionality();
 
     return 0;
 }
